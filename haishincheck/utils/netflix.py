1a57dcd80da6e807c9c9efb49d1f0a3d74540cff
toshiro245 <tt1118rou56@gmail.com>
2022-01-05 00:09:57 +0900

    make it better4

--- haishincheck/utils/netflix.py
+++ haishincheck/utils/netflix.py
@@ -20,24 +20,24 @@ def netflix_scraping(driver, title):
         
         base_url = "https://www.netflix.com/browse"
         driver.get(base_url)
-        time.sleep(5)
+        time.sleep(3)
 
         #ログイン
         login = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/div/div/div[1]/form/div[1]/div[1]/div/label/input')
         login.send_keys(Id)
-        time.sleep(2)
+        time.sleep(1)
 
         login = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/div/div/div[1]/form/div[2]/div[1]/div/label/input')
         login.send_keys(Pass)
-        time.sleep(2)
+        time.sleep(1)
 
         driver.find_element(By.XPATH, '/html/body/div[1]/div/div[3]/div/div/div[1]/form/button').click()
-        time.sleep(2)
+        time.sleep(1)
 
         #アカウントの選択
         driver_wait = WebDriverWait(driver,10)
         driver_wait.until(expected_conditions.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div/div[1]/div[1]/div[2]/div/div/ul/li[1]/div/a/span'))).click()
-        time.sleep(2)
+        time.sleep(1)
 
         page_url = f'https://www.netflix.com/search?q={title}'
         driver.get(page_url)
