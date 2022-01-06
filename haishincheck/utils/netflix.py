5ddaf1edbb735ea55376f7343790f740bb5b22a4
toshiro245 <tt1118rou56@gmail.com>
2022-01-06 13:42:53 +0900

    make it better 1342

--- haishincheck/utils/netflix.py
+++ haishincheck/utils/netflix.py
@@ -54,9 +54,9 @@ def netflix_scraping(driver, title):
     # driver_wait = WebDriverWait(driver,10)
     # driver_wait.until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, 'ul.choose-profile > li:first-of-type > div'))).click()
     
-    # profile_url = driver.find_element(By.CSS_SELECTOR, 'ul.choose-profile > li:first-of-type a').get_attribute('href')
-    driver.find_element(By.CSS_SELECTOR, 'ul.choose-profile > li:first-of-type > div').click()
-    # driver.get(profile_url)
+    profile_url = driver.find_element(By.CSS_SELECTOR, 'ul.choose-profile > li:first-of-type a').get_attribute('href')
+    # driver.find_element(By.CSS_SELECTOR, 'ul.choose-profile > li:first-of-type > div').click()
+    driver.get(profile_url)
     time.sleep(2)
 
     test = 'e'
