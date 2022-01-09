1e70154445346e73492c8c496fd18cb5603b300b
toshiro245 <tt1118rou56@gmail.com>
2022-01-09 14:34:59 +0900

    make it better

--- haishincheck/utils/abema.py
+++ haishincheck/utils/abema.py
@@ -10,16 +10,20 @@ def abema_scraping(driver, title):
     try:
         input_title = title_convert(title)
         
-        # page_url = f"https://abema.tv/search?q={title}"
-        page_url = f"https://abema.tv/"
-        driver.get(page_url)
-        time.sleep(5)
+        
+        homepage_url = f"https://abema.tv/"
+        driver.get(homepage_url)
+        time.sleep(3)
         driver.find_element(By.CSS_SELECTOR, 'div.c-application-GDPRContainer__check_area > label').click()
-        time.sleep(2)
+        time.sleep(1)
         driver.find_element(By.CSS_SELECTOR, 'div.c-application-GDPRContainer__button_area > button').click()
-        time.sleep(5)
+        time.sleep(3)
 
-        # driver.find_elements(By.CSS_SELECTOR, 'li.com-search-SearchResultsVideoSection__list-item')
+        page_url = f"https://abema.tv/search?q={title}"
+        driver.get(page_url)
+        time.sleep(3)
+
+        driver.find_elements(By.CSS_SELECTOR, 'li.com-search-SearchResultsVideoSection__list-item')
 
         html = driver.page_source
         soup = BeautifulSoup(html, 'lxml')
